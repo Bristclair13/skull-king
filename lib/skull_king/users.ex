0faf8201fb2895c1f49f016337b75f1715edf9a5
Brianna St Clair <briannastclair13@gmail.com>
2024-02-11 17:10:42 -0500

    worked on test

--- lib/skull_king/users.ex
+++ lib/skull_king/users.ex
@@ -3,39 +3,13 @@ defmodule SkullKing.Users do
 
   alias SkullKing.Users.Repo
   alias SkullKing.Users.User
+  alias SkullKing.PirateNames
 
   mock SkullKing.Users.Repo
 
-  @pirate_names [
-    "William Kidd",
-    "Blackbeard",
-    "Bartholomew Roberts",
-    "Henry Every",
-    "Henry Morgan",
-    "Stede Bonnet",
-    "Thomas Tew",
-    "Anne Bonny",
-    "Bones",
-    "Calico Jack",
-    "Edward Low",
-    "Howell Davis",
-    "Samuel Bellamy",
-    "Admiral",
-    "Archer",
-    "Augie",
-    "Azure",
-    "Bastian",
-    "Belle",
-    "Bertha",
-    "Booty",
-    "Captain",
-    "Captain Jolly",
-    "Caspian"
-  ]
-
   @callback find_or_create(String.t()) :: {:ok, User.t()} | {:error, Ecto.Changeset.t()}
   def find_or_create(google_id) do
-    name = Enum.random(@pirate_names)
+    name = PirateNames.random_name()
 
     case Repo.get(google_id) do
       user when is_struct(user) -> {:ok, user}
--- lib/skull_king/users_test.exs
+++ lib/skull_king/users_test.exs
@@ -7,12 +7,21 @@ defmodule SkullKing.UsersTest do
   test "find_or_create/1" do
     user = build(:user)
 
+    # user doesn't exist
     Repo.Mock
     |> expect(:get, fn "google_id" ->
       nil
     end)
     |> expect(:create, fn %{name: _name, google_id: "google_id"} ->
       {:ok, user}
+    end)
+
+    assert {:ok, ^user} = Users.find_or_create("google_id")
+
+    # user already exist
+    Repo.Mock
+    |> expect(:get, fn "google_id" ->
+      user
     end)
 
     assert {:ok, ^user} = Users.find_or_create("google_id")

5e944ae00d8f0a3d629196c809375e1a16eac7c3
Brianna St Clair <briannastclair13@gmail.com>
2024-05-08 21:54:24 -0600

    worked on deck testing

--- lib/skull_king/games/deck_test.exs
+++ lib/skull_king/games/deck_test.exs
@@ -3,16 +3,36 @@ defmodule SkullKing.Games.DeckTest do
 
   alias SkullKing.Games.Deck
 
-  @pirate %Deck.Card{special: :pirate}
-  @yellow_5 %Deck.Card{color: :yellow, value: 5}
   @black_10 %Deck.Card{color: :black, value: 10}
+  @black_14 %Deck.Card{color: :black, value: 14}
+  @black_3 %Deck.Card{color: :black, value: 3}
   @black_5 %Deck.Card{color: :black, value: 5}
+  @mermaid %Deck.Card{special: :mermaid}
+  @pirate %Deck.Card{special: :pirate}
+  @purple_11 %Deck.Card{color: :purple, value: 11}
+  @purple_14 %Deck.Card{color: :purple, value: 14}
+  @skull_king %Deck.Card{special: :skull_king}
   @surrender %Deck.Card{id: 1, value: 0, special: :surrender}
   @surrender_2 %Deck.Card{id: 2, value: 0, special: :surrender}
-  @black_3 %Deck.Card{color: :black, value: 3}
-  @skull_king %Deck.Card{special: :skull_king}
-  @mermaid %Deck.Card{special: :mermaid}
-  @purple_11 %Deck.Card{color: :purple, value: 11}
+  @yellow_14 %Deck.Card{color: :yellow, value: 14}
+  @yellow_5 %Deck.Card{color: :yellow, value: 5}
+
+  test "deal/2" do
+    round = build(:round)
+    %{id: user_1_id} = user_1 = build(:user)
+    %{id: user_2_id} = user_2 = build(:user)
+    %{id: user_3_id} = user_3 = build(:user)
+
+    users = [user_1, user_2, user_3]
+
+    assert %{
+             ^user_1_id => [%SkullKing.Games.Deck.Card{user_id: ^user_1_id}],
+             ^user_2_id => [%SkullKing.Games.Deck.Card{user_id: ^user_2_id}],
+             ^user_3_id => [%SkullKing.Games.Deck.Card{user_id: ^user_3_id}]
+           } = Deck.deal(round, users)
+
+    round = build(:round, number: 3) |> dbg()
+  end
 
   test "allowed_cards/2" do
     # my_cards = []
@@ -88,7 +108,7 @@ defmodule SkullKing.Games.DeckTest do
 
     # mermaid beats skull king
 
-    cards_played = [@mermaid, @skull_king]
+    cards_played = [@skull_king, @mermaid]
     assert @mermaid == Deck.winning_card(cards_played)
 
     # mermaid beats value cards
@@ -115,5 +135,53 @@ defmodule SkullKing.Games.DeckTest do
 
     cards_played = [@yellow_5, @purple_11]
     assert @yellow_5 == Deck.winning_card(cards_played)
+
+    # non special card loses even if played first
+
+    cards_played = [@yellow_14, @skull_king]
+    assert @skull_king == Deck.winning_card(cards_played)
+
+    # higher value wins when color is the same
+
+    cards_played = [@yellow_5, @yellow_14]
+    assert @yellow_14 == Deck.winning_card(cards_played)
+  end
+
+  test "bonus_points_for_trick/1" do
+    # 40 bonus points for taking skull king with mermaid
+
+    cards_played = [@mermaid, @skull_king]
+
+    assert 40 == Deck.bonus_points_for_trick(cards_played)
+
+    # 30 bonus points for taking pirate with skull king
+
+    cards_played = [@pirate, @skull_king]
+
+    assert 30 == Deck.bonus_points_for_trick(cards_played)
+
+    # 20 bonus points for taking mermaid with a pirate
+
+    cards_played = [@mermaid, @pirate]
+
+    assert 20 == Deck.bonus_points_for_trick(cards_played)
+
+    # 20 points for capturing black 14
+
+    cards_played = [@skull_king, @black_14]
+
+    assert 20 == Deck.bonus_points_for_trick(cards_played)
+
+    # 10 points for capturing non black 14
+
+    cards_played = [@skull_king, @yellow_14]
+
+    assert 10 == Deck.bonus_points_for_trick(cards_played)
+
+    # points will add together
+
+    cards_played = [@pirate, @purple_14, @yellow_14]
+
+    assert 20 == Deck.bonus_points_for_trick(cards_played)
   end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,8 +5,16 @@ defmodule SkullKing.Factory do
 
   def user_factory() do
     %SkullKing.Users.User{
+      id: UXID.generate!(prefix: "users"),
       name: PirateNames.random_name(),
       google_id: Ecto.UUID.generate()
+    }
+  end
+
+  def round_factory() do
+    %SkullKing.Games.Round{
+      id: UXID.generate!(prefix: "round"),
+      number: 1
     }
   end
 end
